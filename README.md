41a087289156abf2b1fe3df2d6422afc2e14a205
Krantz-XRF <Krantz.XRF@outlook.com>
2020-09-14 13:08:15 +0800

    [dev] setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Memory Manager
+
+A garbage-collected memory manager written in Rust.
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+/*
+ * garbage-collected memory manager in Rust
+ * Copyright (C) 2020  Xie Ruifeng
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+//! garbage-collected memory manager
+#![no_std]
+#![deny(missing_docs)]
+#![deny(dead_code)]
+#![deny(unused_imports)]
+#![deny(unused_variables)]
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+mod tests {}
